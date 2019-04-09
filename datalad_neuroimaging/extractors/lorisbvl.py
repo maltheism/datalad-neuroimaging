d829a7880e8f6950cc18b0a405051cd321a7dc77
Alizée Bellerose <misotheism@users.noreply.github.com>
2019-04-09 16:04:31 -0400

    fix potential memory leak and json not valid

--- datalad_neuroimaging/extractors/lorisbvl.py
+++ datalad_neuroimaging/extractors/lorisbvl.py
@@ -27,32 +27,41 @@ class LorisExtractor(BaseMetadataExtractor):
                 files.append(f)
             else:
                 # open file
-                file = open(str(f), 'r')
-                # read file content
-                file_content = file.read()
-                # dictionary from file_content
-                instrument_dict = json.loads(file_content)
+                try:
+                    file = open(str(f), 'r')
+                    # read file content
+                    try:
+                        file_content = file.read()
+                        # dictionary from file_content
+                        try:
+                            instrument_dict = json.loads(file_content)
 
-                # retrieve candidate & visit from Meta.
-                candidate = instrument_dict['Meta']['Candidate']
-                visit = instrument_dict['Meta']['Visit']
+                            # retrieve candidate & visit from Meta.
+                            candidate = instrument_dict['Meta']['Candidate']
+                            visit = instrument_dict['Meta']['Visit']
 
-                # first key in instrument dictionary
-                first_key = next(iter(instrument_dict))
+                            # first key in instrument dictionary
+                            first_key = next(iter(instrument_dict))
 
-                # Add Candidate & Visit to Instrument dictionary
-                instrument_dict[first_key]['Candidate'] = candidate
-                instrument_dict[first_key]['Visit'] = visit
+                            # Add Candidate & Visit to Instrument dictionary
+                            instrument_dict[first_key]['Candidate'] = candidate
+                            instrument_dict[first_key]['Visit'] = visit
 
-                # Remove Meta from instrument dictionary
-                instrument_dict.pop('Meta', None)
+                            # Remove Meta from instrument dictionary
+                            instrument_dict.pop('Meta', None)
 
-                # Add to collection dictionary based on candidate_visit as index.
-                if candidate + '_' + visit in self.collection:
-                    self.collection[candidate + '_' + visit][first_key] = instrument_dict[first_key]
-                else:
-                    self.collection[candidate + '_' + visit] = {first_key: ''}
-                    self.collection[candidate + '_' + visit][first_key] = instrument_dict[first_key]
+                            # Add to collection dictionary based on candidate_visit as index.
+                            if candidate + '_' + visit in self.collection:
+                                self.collection[candidate + '_' + visit][first_key] = instrument_dict[first_key]
+                            else:
+                                self.collection[candidate + '_' + visit] = {first_key: ''}
+                                self.collection[candidate + '_' + visit][first_key] = instrument_dict[first_key]
+                        except ValueError:
+                            continue
+                    finally:
+                        file.close()
+                except IOError:
+                    continue
 
         return {}, self.get_minc_metadata(files)
 
